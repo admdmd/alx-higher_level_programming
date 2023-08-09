40ba78fc3a6981bcaae5f64fe5752fc2c749dd5a
ademide <ademideadeboye@gmail.com>
2023-08-09 15:40:37 -0700

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for alphabet in range(ord('a'), ord('z') + 1):
-    print("{}".format(chr(alphabet)), end='')
+for c in range(ord('a'), ord('z') + 1):
+    print("{:c}".format(c), end="")
