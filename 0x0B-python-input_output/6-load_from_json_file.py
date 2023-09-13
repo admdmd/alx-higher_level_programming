fbf6724dc6dd9bec1ac3201c408af08c25b59b41
ademide <ademideadeboye@gmail.com>
2023-09-12 21:53:36 -0700

    6-load_from_json_file.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+creates an Object from a JSON file
+"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """crate object from a JSON file """
+    with open(filename, 'xim', encoding='utf-8') as f:
+        return json.load(f)
