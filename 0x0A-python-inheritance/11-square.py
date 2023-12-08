e69025a936b384dfe8e770dbc3ce7bca7f99d539
Your Name <you@example.com>
2023-12-08 03:07:03 +0000

    11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -5,9 +5,10 @@ Defines a rectangle subclass square
 
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
     """Represent a square."""
-    
+
     def __init__(self, size):
         """Initialize a new square.
 
