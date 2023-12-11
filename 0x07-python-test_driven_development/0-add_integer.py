bfaf39eae3ff6c275519af38ae58ba2dfa00a881
Your Name <you@example.com>
2023-12-11 20:16:01 +0000

    Test Driven

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+#!usr/bin/python3
+"""Defines an integer addition function."""
+
+def add_integer(a, b=98):
+    """Returns the addition of a and b,
+    or an error if a and b are not integers or floats
+    """
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
