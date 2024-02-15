32b2f8cc8859c6650a978e9a8737f23b7efdb33b
Your Name <you@example.com>
2024-02-15 02:24:40 +0000

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
-const arg = process.argv[2];
-console.log(arg ? arg : 'No argument');
+if (process.argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}
