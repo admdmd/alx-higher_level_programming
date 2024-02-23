7c10924edaaa06b8d099686f254a488176c0ba3a
Your Name <you@example.com>
2024-02-23 21:47:28 +0000

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,34 +1,24 @@
 #!/usr/bin/python3
 """
-Script displays all values in the states table:
-hbtn_0e_0_usa where name matches the argument
+Displays all values in the states table of the database hbtn_0e_0_usa
+whose name matches that supplied as argument.
+Usage: ./2-my_filter_states.py <mysql username> \
+                                <mysql password> \
+                                <database name> \
+                                <state name searched>
 """
-import MySQLdb
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    us = sys.argv[1]
-    ps = sys.argv[2]
-    dbs = sys.argv[3]
-    name = sys.argv[4]
-
-    try:
-   # Explicitly set host to "localhost" and use default socket
-        db = MySQLdb.connect(
-            host="localhost", port=3306, user=us, passwd=ps, db=dbs, unix_socket="/var/run/mysqld/mysqld.sock"
-        )
-        cursor = db.cursor()
-        cursor.execute(
-            "SELECT * FROM states WHERE name LIKE BINARY \
-            '{}' ORDER BY id ASC".format(name))
-
-        rows = cursor.fetchall()
-
-        for row in rows:
-            print(row)
-        cursor.close()
-        db.close()
-
-    except MySQLdb.Error as e:
-        print("Error connecting to MySQL:", e)
-        sys.exit(1)
+    db = MySQLdb.connect(user=sys.argv[1], port=3306, host="localhost",
+                         passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+    id ASC".format(sys.argv[4]))
+    states = c.fetchall()
+    for state in states:
+        if state[1] == sys.argv[4]:
+            print(state)
+    c.close()
+    db.close()
