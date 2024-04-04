96dd9bf339cd18635cbf8ce3c66d68a5f1238fee
Your Name <you@example.com>
2024-04-04 13:54:45 +0000

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,25 +1,46 @@
 #!/usr/bin/node
 
 const request = require('request');
-const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+const movieId = process.argv[2];
 
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
-        }
-      });
+if (!movieId || isNaN(movieId)) {
+  console.error('Usage: ./101-starwars_characters.js <movie_id>');
+  process.exit(1);
+}
+
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request(apiUrl, function (error, response, body) {
+  try {
+    if (!error && response.statusCode === 200) {
+      const film = JSON.parse(body);
+      const charactersUrls = film.characters;
+
+      const fetchCharacterName = (url) => {
+        return new Promise((resolve, reject) => {
+          request(url, (err, res, b) => {
+            if (!err && res.statusCode === 200) {
+              const character = JSON.parse(b);
+              resolve(character.name);
+            } else {
+              reject(err);
+            }
+          });
+        });
+      };
+
+      // Fetch character names in order
+      charactersUrls.reduce((sequence, url) => {
+        return sequence.then(() => {
+          return fetchCharacterName(url).then((name) => {
+            console.log(name);
+          });
+        });
+      }, Promise.resolve());
+    } else {
+      console.error('Error:', error);
     }
+  } catch (parseError) {
+    console.error('Error parsing JSON:', parseError);
   }
 });
