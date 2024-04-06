8cae4441e9e8eed46bccaad0b4ba7a6aa5b89f18
Your Name <you@example.com>
2024-04-06 18:23:11 +0000

    3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,17 +1,12 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor(w, h) {
-        if (w <= 0 || h <= 0) {
-            // If width or height is not a positive integer, create an empty object
-            this.width = null;
-            this.height = null;
-        } else {
-            // Initialize instance attributes width and height
-            this.width = w;
-            this.height = h;
-        }
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
     }
+  }
 
     print () {
     for (let i = 0; i < this.height; i++) {
