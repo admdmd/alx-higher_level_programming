e60bccb6e891c64a9e1247aaf01459914f250a7a
Your Name <you@example.com>
2024-04-09 03:40:45 +0000

    100-script.js

---
+++ 100-script.js
@@ -0,0 +1,5 @@
+// 100-script.js
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('header').style.color = '#FF0000';
+});
---
+++ 6-script.js
@@ -0,0 +1,6 @@
+// Update the texts of header tag with a new header when the
+// DIV#update_header tag is clicked
+
+$('div#update_header').click(function () {
+    $('header').text('New Header!!!');
+  });
